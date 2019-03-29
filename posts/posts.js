c1fc5ccd95369a4553f33e1fcdf4cee81fb8111e
FioraPellerin <ifthe1201@naver.com>
2019-03-29 10:00:44 +0900

    로고 변경/기본 페이지 추가

---
+++ posts/posts.js
@@ -0,0 +1,21 @@
+const posts = 
+{
+	"category": {
+		"type": "single",
+		"href": "/posts/category/",
+		"icon": "fa fa-book",
+		"posts": []
+	},
+	"dropdown": {
+		"type": "dropdown",
+		"href": "#",
+		"icon": "fa fa-book",
+		"posts": {
+			"sub-category": {
+				"href": "/posts/dropdown/subcategory/",
+				"icon": "icon-note",
+				"posts": []
+			}
+		}
+	}
+}
