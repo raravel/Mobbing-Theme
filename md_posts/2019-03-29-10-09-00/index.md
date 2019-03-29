48bf3e29cd06babda2eac71f99834550c4163ebd
FioraPellerin <ifthe1201@naver.com>
2019-03-29 10:10:55 +0900

    테스트 포스팅

---
+++ md_posts/2019-03-29-10-09-00/index.md
@@ -0,0 +1,19 @@
+# Test Posting
+## sub title 1
+
+```c
+#include <stdio.h>
+int main ( void )
+{
+	printf("Hello World\n");
+	return 0;
+}
+```
+
+## sub title 2
+
+![img-1](imgs/image-1.png)
+
+## sub title 3
+
+Noting.
--- posts/posts.js
+++ posts/posts.js
@@ -14,7 +14,13 @@ const posts =
 			"sub-category": {
 				"href": "/posts/dropdown/subcategory/",
 				"icon": "icon-note",
-				"posts": []
+				"posts": [
+					{
+						"href": "/posts/dropdown/subcategory/2019-03-29-10-09-00/",
+						"icon": "icon-note",
+						"title": "Test Posting"
+					}
+				]
 			}
 		}
 	}
